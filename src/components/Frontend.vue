8adc15c5db2dd73ed152bfb9f2120c7047e10d98
nerocodes <neropaulej@gmail.com>
2019-09-19 03:48:51 +0100

    Completed frontend section

---
+++ src/components/Frontend.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="frontend">
+    Vuejs, Reactjs, JQuery, Bootstrap, Materialize
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Frontend'
+}
+</script>
